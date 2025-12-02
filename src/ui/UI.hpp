a8f5dc010b2b6f15dbb37bade9cfc2449b8ed830
Zane Myers <zrmyers554@gmail.com>
2025-12-01 23:14:26 -0600

    initial nineslice interface

--- src/ui/UI.hpp
+++ src/ui/UI.hpp
@@ -233,6 +233,22 @@ namespace UI {
         public:
             NineSlice();
 
+            //! Set the NineSlice style.
+            void SetStyle(const NineSliceStyle& style);
+
+            //! Calculate the size of the element.
+            //!
+            //! @param[in] parent_size The absolute size of the parent element.
+            void CalculateSize(glm::vec2 parent_size) override;
+
+            //! Calculate the position of the element
+            //!
+            //! @param[in] parent_size The absolute size of the parent element.
+            //! @param[in] parent_position The absolute position of the parent element.
+            void CalculatePosition(glm::vec2 parent_size, glm::vec2 parent_position) override;
+
+            void UpdateGraphics(ECS::Registry& registry, glm::vec2 screenSize, int depth) override;
+
         private:
 
             NineSliceStyle m_style;
