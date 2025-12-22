1e6998543bcb2a7f828497b70591b695d4cab49f
Zane Myers <zrmyers554@gmail.com>
2025-12-21 18:31:39 -0600

    more work on character creator

--- src/character/Character.hpp
+++ src/character/Character.hpp
@@ -7,10 +7,64 @@
 #include <glm/ext/vector_float4.hpp>
 #include <memory>
 #include <string>
+#include <unordered_map>
 #include <vector>
 
 namespace Character {
 
+    // Character is composed of multiple parts.
+    enum class PartId : uint8_t {
+        HEAD = 0,
+        UPPER_TORSO,
+        HIP,
+        RIGHT_ARM,
+        LEFT_ARM,
+        RIGHT_LEG,
+        LEFT_LEG,
+        RIGHT_FOOT,
+        LEFT_FOOT,
+        RIGHT_HAND,
+        LEFT_HAND,
+        EYES,
+        EYELASHES,
+        EYEBROW,
+        HAIR,
+        BEARD,
+
+        // Add parts before
+        PART_COUNT
+    };
+
+    static size_t PartIdToIndex(PartId part_id) {
+        return static_cast<size_t>(part_id);
+    }
+
+    // Represents a part of the character
+    struct BodyPart {
+        PartId part_id; // Identifier for the part.
+        std::shared_ptr<Graphics::Mesh> m_p_mesh; // graphical representation of part with mesh.
+        int16_t m_max_health; // maximum possible health of the part.
+        int16_t m_current_health; // current health of the part.
+    };
+
+    struct CharacterModel {
+        std::unordered_map<PartId, std::shared_ptr<Graphics::Mesh>> m_healthy;
+        std::unordered_map<PartId, std::shared_ptr<Graphics::Mesh>> m_destroyed_fresh;
+        std::unordered_map<PartId, std::shared_ptr<Graphics::Mesh>> m_destroyed_scarred;
+    };
+
+    // This class is responsible for creating various instances of characters based on data from configuration file.
+    class Creator {
+
+        public:
+
+            Creator(const std::string& filename);
+
+        private:
+
+            std::unordered_map<std::string, CharacterModel> m_model_types;
+
+    };
     // This class represents a character in the game. Each character is assumed to be human, with the standard set of
     // body parts that a human is composed of. (consider moving definition to other class)
     //
@@ -26,38 +80,11 @@ namespace Character {
                 FEMALE
             };
 
-            // Character is composed of multiple parts.
-            enum class PartId : uint8_t {
-                HEAD = 0,
-                UPPER_TORSO,
-                HIP,
-                RIGHT_ARM,
-                LEFT_ARM,
-                RIGHT_LEG,
-                LEFT_LEG,
-                RIGHT_FOOT,
-                LEFT_FOOT,
-                RIGHT_HAND,
-                LEFT_HAND,
-                EYES,
-                EYELASHES,
-                EYEBROW,
-                HAIR,
-                BEARD,
-
-                // Add parts before
-                PART_COUNT
-            };
-
             static constexpr size_t NUM_PARTS = static_cast<size_t>(PartId::PART_COUNT);
 
             Character() = default;
 
         private:
-
-            static size_t PartIdToIndex(PartId part_id) {
-                return static_cast<size_t>(part_id);
-            }
 
             // Customizable traits of character.
             Sex m_sex {Sex::MALE};
@@ -68,15 +95,8 @@ namespace Character {
             std::string m_hair_style; // if empty none
             std::string m_beard_style; // if empty none
 
-            // Represents a part of the character
-            struct BodyPart {
-                PartId part_id; // Identifier for the part.
-                std::shared_ptr<Graphics::Mesh> m_p_mesh; // graphical representation of part with mesh.
-                int16_t m_max_health; // maximum possible health of the part.
-                int16_t m_current_health; // current health of the part.
-            };
-
             // set of parts that make up character.
             std::vector<BodyPart> m_parts;
     };
+
 }
